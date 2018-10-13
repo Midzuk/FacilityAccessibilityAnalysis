bdf08cc9e9951c131d1e0505b3fbf9b0d857da21
Midzuki <ucdmzk@icloud.com>
2018-10-13 17:07:41 +0900

    修正

---
+++ FacilityAccessibilityAnalysis.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(dplyr)
+library(magrittr)
+
+data <- read_csv("ignore/input/data.csv")
+
+data %<>%
+  rename(sample_id = "SAMPLEID",
+         facility_use = "Q2",
+         income = "Q23",
+         sex = "SEX",
+         age = "AGE",
+         child = "CHILD",
+         one_line_distance = "DISTANCE1") %>%
+  select(sample_id,
+         facility_use,
+         income,
+         sex,
+         age,
+         child,
+         one_line_distance) %>%
+  mutate(facility_use = case_when(facility_use == 2 ~ 0,
+                                  facility_use == 1 ~ 1),
+         income = case_when(income >= 1 & income <= 10 ~ income * 1000000 - 500000,
+                            income == 11 ~ 12500000,
+                            income == 12 ~ 17500000),
+         sex = sex - 1,
+         child = child - 1,
+         one_line_distance = one_line_distance * 1000)
+
+dist_data <- read_csv("ignore/input/rawdata_distance.csv")
+
+dist_data %<>%
+  mutate(distance = dist_link + dist_org *0 + dist_dest *0)
+
+data %<>%
+  left_join(dist_data, by = "sample_id") %>%
+  filter(!is.na(income) & !is.na(distance))
+
+result <- glm(facility_use ~ distance + income + sex, data, family = binomial(link = "logit"))
+summary(result)
+predict(result, type = "response")
+
